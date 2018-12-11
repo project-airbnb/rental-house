c07938c059317a13aa5adf143706cc316261b90b
nguyenhai9112 <nguyenhai9112@gmail.com>
2018-12-11 11:25:34 +0700

    update list post rent

--- src/main/java/com/PKHS/airbnb/controller/PostRentController.java
+++ src/main/java/com/PKHS/airbnb/controller/PostRentController.java
@@ -1,8 +1,25 @@
 package com.PKHS.airbnb.controller;
 
+import com.PKHS.airbnb.model.PostRent;
+import com.PKHS.airbnb.service.PostRentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/post")
 public class PostRentController {
+    @Autowired
+    private PostRentService postRentService;
+
+    @GetMapping
+    public String PostController(Model model) {
+        Iterable<PostRent> postRents = this.postRentService.findAll();
+        model.addAttribute("postRents", postRents);
+        return "post-rent/list";
+    }
+
+
 }
