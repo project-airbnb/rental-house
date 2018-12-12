b688a0d45064aa9b28a4ca414d89b2edc5d7c93d
Khanh Hoang Nam <chienbinhbattu9xpro@gmail.com>
2018-12-12 15:25:09 +0700

    Add test newsrent controller

---
+++ src/test/java/com/PKHS/airbnb/controller/NewsRentControllerTest.java
@@ -0,0 +1,49 @@
+package com.PKHS.airbnb.controller;
+
+import com.PKHS.airbnb.model.NewsRent;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class NewsRentControllerTest {
+    @Autowired
+    private NewsRentController newsRentController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void listNewsrents() throws Exception {
+        assertNotNull(mockMvc);
+        assertNotNull(newsRentController);
+        mockMvc
+                .perform(get("/new-rent"))
+                .andExpect(status().isOk());
+
+    }
+}
